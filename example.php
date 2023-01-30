c40ceece0708ff0842fe4918f910ff4144523d9c
Timothy Soladoye <tim.soladoye@gmail.com>
2023-01-30 09:57:28 +0100

    process form

--- example.php
+++ example.php
@@ -4,7 +4,7 @@ require_once 'vendor/autoload.php';
 
 use Timoye\Paystack\PaystackTerminal;
 
-function formFields(Request  $request){
+function formFields(Request $request){
     $secret_key='xxxx -xxx';//get key from env or config
     try{
         $form_fields_response = (new PaystackTerminal($secret_key))
@@ -19,6 +19,24 @@ function formFields(Request  $request){
     }
     return response()->json($form_fields_response)
         ->withHeaders((new PaystackTerminal($secret_key))->getHeaderArray($form_fields_response));
+
+}
+
+function processForm(Request $request){
+    $secret_key='xxxx -xxx';//get key from env or config
+    try{
+        $process_form_response = (new PaystackTerminal($secret_key))
+            ->authCheck($request)
+            ->setData($request)
+            ->processFormResponse();
+    }catch(\Exception $e){
+        return [
+            'status'=>'fail',
+            'details'=>$e->getMessage()
+        ];
+    }
+    return response()->json($process_form_response)
+        ->withHeaders((new PaystackTerminal($secret_key))->getHeaderArray($process_form_response));
 
 }
 
