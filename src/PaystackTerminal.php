6d5ce6e608493b298f724089b9b352d3233ee04c
Timothy Soladoye <tim.soladoye@gmail.com>
2023-01-30 09:32:22 +0100

    clean auth check

--- src/PaystackTerminal.php
+++ src/PaystackTerminal.php
@@ -26,19 +26,29 @@ class PaystackTerminal
 
         $request_body = json_encode($request->all(), JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
         $body_to_hash = $method != 'GET' ? $request_body : '';
-        $hashed_body = md5($body_to_hash);
+        $hashed_body = $this->createMD5Hash($body_to_hash);
 
-        $to_encode = "$method
-$this->path
-$date
-$hashed_body";
-        $to_encode = trim($to_encode);
+        $hashed=$this->createHMACHash([$method,$this->path,$date,$hashed_body]);
 
-        $hashed = hash_hmac('sha512', $to_encode, $this->secret_key, false);
         if ($hashed != $header_auth) {
             throw new \Exception('Auth Check failed');
         }
         return $this;
+    }
+
+    public function createHMACHash($array)
+    {
+        /*        $to_encode = "$method
+$this->path
+$date
+$hashed_body";*/
+        $to_encode=implode("\n", $array);
+        $to_encode = trim($to_encode);
+        return hash_hmac('sha512', $to_encode, $this->secret_key, false);
+    }
+
+    public function createMD5Hash($body_to_hash){
+        return md5($body_to_hash);
     }
 
     public function generateReference()
